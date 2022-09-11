8e8bbbdd1aa9eb78885d7b4983a6bc187e1f96f7
Sky233ml <64515462+sky130@users.noreply.github.com>
2022-09-11 17:01:56 +0800

    Update Hitokoto.java

--- src/main/java/ml/sky233/hitokoto/Hitokoto.java
+++ src/main/java/ml/sky233/hitokoto/Hitokoto.java
@@ -23,18 +23,29 @@ public class Hitokoto {
 
     //返回编码
     public static final String CHARSET_DEFAULT = "UTF-8";
-    public static final int LENGTH_MAX = 0;
-    public static final int LENGTH_MIN = 0;
+    public static final int LENGTH_MAX_DEFAULT = 0;
+    public static final int LENGTH_MIN_DEFAULT = 0;
 
     //设置类型
     public static final int URL = 0;
     public static final int TYPE = 3;
-    public static final int CHARSET = 2;
-    public static final int MIN_LENGTH = 1;
-    public static final int MAX_LENGTH = 4;
-    public static final int TOKEN = 5;
+    public static final int CHARSET = 6;
+    public static final int MIN_LENGTH = 9;
+    public static final int MAX_LENGTH = 7;
+    public static final int TOKEN = 8;
+    public static final int COMMENT = 10;
+    public static final int SCORE = 11;
+    //public static final int UUID = 1;
+    //  public static final int HITOKOTO = 2;
+    //  public static final int FROM = 4;
+    //  public static final int FROM_WHO = 5;
 
-    //句子数组内容
+    //数组内容
+    public static final int LOGIN_STATUS = 0;
+    public static final int LOGIN_NAME = 1;
+    public static final int LOGIN_EMAIL = 2;
+    public static final int LOGIN_ID = 3;
+    public static final int LOGIN_TOKEN = 4;
     public static final int ID = 0;
     public static final int UUID = 1;
     public static final int HITOKOTO = 2;
@@ -54,6 +65,27 @@ public class Hitokoto {
     public static final String RESULT_ERROR = "-1";//参数配置错误返回
 
     //用户喜欢列表长度和偏移
-    public static final int USER_LIKE_LIMIT = 20;
-    public static final int USER_LIKE_OFFSET = 0;
+    public static final int LIKE_LIMIT_DEFAULT = 20;
+    public static final int LIKE_OFFSET_DEFAULT = 0;
+
+    //历史记录类型
+    public static final int HISTORY_DEFAULT = 0;
+    public static final int HISTORY_ALL = 0;
+    public static final int HISTORY_PENDING = 1;
+    public static final int HISTORY_REFUSE = 2;
+    public static final int HISTORY_ACCEPT = 3;
+
+    //设置类型
+    public static final int SETTING_GLOBAL = 0;
+    public static final int SETTING_HITOKOTO_APPENDED = 1;
+    public static final int SETTING_HITOKOTO_REVIEWED = 2;
+    public static final int SETTING_POLL_CREATED = 3;
+    public static final int SETTING_POLL_RESULT = 4;
+    public static final int SETTING_POLL_DAILY_REPORT = 5;
+
+    //提交数据类型
+    public static final int SUMMARY_TOTAL = 1;
+    public static final int SUMMARY_PENDING = 2;
+    public static final int SUMMARY_REFUSE = 3;
+    public static final int SUMMARY_ACCEPT = 4;
 }
